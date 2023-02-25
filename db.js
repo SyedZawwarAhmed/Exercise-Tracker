2cf7b91344cb9bb5e36419c28eea377e748c56f6
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-02-25 17:57:36 +0500

    update db.js

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import mongoose from "mongoose";
 import pkg from 'mongoose';
 const { connect, connection } = pkg;
 
 const app = express();
 const port = process.env.PORT || 5000;
+mongoose.set('strictQuery', false)
 
 // Connection URL
 const url = process.env.MONGO_URL;
