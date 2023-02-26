2b0d5cad8ee742b151d26dcf84d8f0edc7142116
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-02-26 14:52:58 +0500

    update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -31,14 +31,14 @@ userRouter
 userRouter.route("/:_id/exercises").post(async (req, res, next) => {
   try {
     const { description, duration, date } = req.body;
-    const dateString = date ? new Date(date) : new Date();
+    const dateObject = date ? new Date(date) : new Date();
     const id = req.params._id;
     const user = await User.findById(id);
     const newExercise = new Exercise({
       username: user.username,
       description,
       duration,
-      date: dateString.toDateString(),
+      date: dateObject,
       userId: id,
     });
     await newExercise.save();
@@ -48,16 +48,28 @@ userRouter.route("/:_id/exercises").post(async (req, res, next) => {
       username: newExercise.username,
       description: newExercise.description,
       duration: newExercise.duration,
-      date: newExercise.date,
+      date: newExercise.date.toDateString(),
     });
   } catch (err) {
     next(err);
   }
 });
 
-userRouter.route("/:_id/logs").get(async (req, res, next) => {
+userRouter.route("/:_id/logs/:from?/:to?/:limit?").get(async (req, res, next) => {
   try {
+
     const logs = await Exercise.find({ userId: req.params._id });
+    const filteredLogs = logs.filter((log, index) => {
+      if (req.params.from && req.params.to) {
+        const from = new Date(req.params.from)
+        const to = new Date(req.params.to)
+        return log.date >= from && log.date <= to
+      } else if (req.params.limit) {
+        return index < limit
+      } else {
+        return true
+      }
+    })
     const username = logs[0].username;
     const count = logs.length;
     const _id = req.params._id;
@@ -65,10 +77,10 @@ userRouter.route("/:_id/logs").get(async (req, res, next) => {
       username,
       count,
       _id,
-      log: logs.map((log) => ({
+      log: filteredLogs.map((log) => ({
         description: log.description,
         duration: log.duration,
-        date: log.date,
+        date: log.date.toDateString(),
       })),
     };
     res.statusCode = 200;
